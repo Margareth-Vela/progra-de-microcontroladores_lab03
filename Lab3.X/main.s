082b721afabe7a1983dc5add5ca0829cea6d7b22
Margareth-Vela <68076747+Margareth-Vela@users.noreply.github.com>
2021-02-16 18:18:51 -0600

    Comentarios en código

--- Lab3.X/main.s
+++ Lab3.X/main.s
@@ -13,32 +13,32 @@ PROCESSOR 16F887
 #include <xc.inc>
 
 ; CONFIG1
-  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
-  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
-  CONFIG  PWRTE = ON            ; Power-up Timer Enable bit (PWRT enabled)
-  CONFIG  MCLRE = OFF           ; RE3/MCLR pin function select bit (RE3/MCLR pin function is digital input, MCLR internally tied to VDD)
-  CONFIG  CP = OFF              ; Code Protection bit (Program memory code protection is disabled)
-  CONFIG  CPD = OFF             ; Data Code Protection bit (Data memory code protection is disabled)
-  CONFIG  BOREN = OFF           ; Brown Out Reset Selection bits (BOR disabled)
-  CONFIG  IESO = OFF            ; Internal External Switchover bit (Internal/External Switchover mode is disabled)
-  CONFIG  FCMEN = OFF           ; Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is disabled)
-  CONFIG  LVP = ON              ; Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+  CONFIG  FOSC = INTRC_NOCLKOUT ; Oscilador interno sin salidas
+  CONFIG  WDTE = OFF            ; WDT disabled (reinicio dispositivo del pic)
+  CONFIG  PWRTE = ON            ; PWRT enabled (espera de 72ms al iniciar)
+  CONFIG  MCLRE = OFF           ; El pin de MCLR se utiliza como I/O
+  CONFIG  CP = OFF              ; Sin protección de código
+  CONFIG  CPD = OFF             ; Sin protección de datos
+  CONFIG  BOREN = OFF           ; Sin reinicio cuándo el voltaje de alimentacion baja de 4v
+  CONFIG  IESO = OFF            ; Reinicio sin cambio de reloj de interno a externo
+  CONFIG  FCMEN = OFF           ; Cambio de reloj externo a interno en caso de fallo
+  CONFIG  LVP = ON              ; Programacion en bajo voltaje permitida
 
 ; CONFIG2
-  CONFIG  BOR4V = BOR40V        ; Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
-  CONFIG  WRT = OFF             ; Flash Program Memory Self Write Enable bits (Write protection off)
+  CONFIG  BOR4V = BOR40V        ; Protección de autoescritura por el programa desactivada
+  CONFIG  WRT = OFF             ; Reinicio abajo de 4V, (BOR21V=2.1V)
 
 ;-------------------------------------------------------------------------------
 ; Variables
 ;-------------------------------------------------------------------------------
-PSECT udata_shr ;share memory
+PSECT udata_shr ;Share memory
     contador : DS 1 ;1 byte
         
 ;-------------------------------------------------------------------------------
 ; Vector Reset
 ;-------------------------------------------------------------------------------
 PSECT resetvector, class=code, delta=2, abs
-ORG 0x0000
+ORG 0x0000   ;Posición 0000h para el reset
 resetvector:
     goto setup
 
@@ -46,10 +46,10 @@ resetvector:
 ; Código Principal 
 ;-------------------------------------------------------------------------------
 PSECT code, delta=2, abs
-ORG 0x000A
+ORG 0x000A ;Posición para el código
  
 tabla:
-    andlw   0x0F
+    andlw   0x0F	; Se utilizan solo los 4 bits menos signficativos 
     addwf   PCL		; PC = offset + PCL 
     retlw   00111111B	;0
     retlw   00000110B	;1
@@ -69,53 +69,53 @@ tabla:
     retlw   01110001B	;F
                                            
 setup:
-    call config_reloj
-    call config_io
-    call config_tmr0
+    call config_reloj	; Configuración del reloj
+    call config_io	; Configuración de I/O
+    call config_tmr0	; Configuración inicial del tmr0
 
 loop:
-    btfsc   PORTA, 0 ;se presiona el push
+    btfsc   PORTA, 0 ; Se presiona el push para incrementar
     call    inc_push 
-    btfsc   PORTA, 1 ;se presiona el push
+    btfsc   PORTA, 1 ; Se presiona el push para decrementar
     call    dec_push
     btfss   T0IF
-    goto    $-5
+    goto    $-5	     ; Revisa si algún push está siendo presionado
     call    reiniciar_tmr0
-    incf    PORTC
+    incf    PORTC    ; Se incrementa el contador del tmr0
     
-    btfsc   PORTE, 0
+    btfsc   PORTE, 0 ;Si la alarma está activa, se apaga
     bcf	    PORTE, 0
-    call    alarma
+    call    alarma   
     goto    loop
     
 ;-------------------------------------------------------------------------------
 ; Sub rutinas de configuración
 ;-------------------------------------------------------------------------------
 config_io:
-    banksel ANSEL ;banco 11
-    clrf    ANSEL
+    banksel ANSEL ;Banco 11
+    clrf    ANSEL ;Pines digitales
     clrf    ANSELH
     
-    banksel TRISA ;banco 01
-    movlw   0xF0
-    movwf   TRISC ;salida del timer 0
-    movlw   0xF0
-    clrf    TRISD ;display 7seg
-    clrf    TRISE
+    banksel TRISA ;Banco 01
+    movlw   0xF0  ;Se utilizan solo los 4 bits menos significativos como salida
+    movwf   TRISC ;Salida del timer 0
+    movlw   0xF0  ;Se utilizan solo los 4 bits menos significativos como salida
+    clrf    TRISD ;Display 7seg
+    clrf    TRISE ;Led de alarma
     
-    banksel PORTA ;banco 00
-    clrf    PORTC ;comenzar contador binario en 0
-    clrf    PORTD ;comenzar contador hexadecimal en 0
-    clrf    PORTE   
-    clrf    contador
+    banksel PORTA ;Banco 00
+    clrf    PORTC ;Comenzar contador binario en 0
+    clrf    PORTD ;Comenzar contador hexadecimal en 0
+    clrf    PORTE ;La alarma está apagada
+    clrf    contador ;Comenzar el contador del display en 0
     return
 
 config_reloj:
     banksel OSCCON
-    bcf	    IRCF2  ;IRCF = 010 250kHz
+    bcf	    IRCF2  ;IRCF = 010 frecuencia=250kHz
     bsf	    IRCF1
     bcf	    IRCF0
-    bsf	    SCS	    ;reloj interno
+    bsf	    SCS	    ;Reloj interno
     return
     
 ;-------------------------------------------------------------------------------
@@ -123,18 +123,18 @@ config_reloj:
 ;-------------------------------------------------------------------------------
 config_tmr0:
     banksel TRISA
-    bcf	    T0CS    ;reloj intero
-    bcf	    PSA	    ;prescaler al TMR0
+    bcf	    T0CS    ;Reloj intero
+    bcf	    PSA	    ;Prescaler al TMR0
     bsf	    PS2
     bsf	    PS1
-    bcf	    PS0	    ;PS = 110  1:128 
+    bcf	    PS0	    ;PS = 110  prescaler = 1:128 
     banksel PORTA
     call reiniciar_tmr0
     return
   
 reiniciar_tmr0:
-    movlw   12
-    movwf   TMR0
+    movlw   12	    ;Número inicial del tmr0
+    movwf   TMR0    
     bcf	    T0IF
     return
 
@@ -142,35 +142,35 @@ reiniciar_tmr0:
 ; Sub rutinas para display
 ;-------------------------------------------------------------------------------
 inc_push:
-    btfsc   PORTA, 0 ;antirebote
+    btfsc   PORTA, 0 ;Antirebote
     goto    $-1
-    incf    contador
+    incf    contador ;Se incrementa la variable de contador
     movwf   contador, 0
     call    tabla
-    movwf   PORTD;se incrementa el display
+    movwf   PORTD;Se incrementa el display
     return
     
 dec_push:
-    btfsc   PORTA, 1 ;antirebote
+    btfsc   PORTA, 1 ;Antirebote
     goto    $-1
-    decf    contador
-    movwf   contador, 0
+    decf    contador ;Se decrementa la variable de contador
+    movwf   contador, 0 
     call    tabla 
-    movwf   PORTD;se decrementa el display 
+    movwf   PORTD;Se decrementa el display 
     return    
 ;-------------------------------------------------------------------------------
 ; Sub rutinas para alarma
 ;-------------------------------------------------------------------------------
 alarma:
-    bcf	    STATUS, 2
-    movwf   contador, 0
-    subwf   PORTC, 0
-    btfsc   STATUS, 2
-    bsf	    PORTE, 0
-    btfsc   STATUS, 2
-    clrf    PORTC
-    btfsc   STATUS, 2
-    call    reiniciar_tmr0
+    bcf	    STATUS, 2  ;Se apaga la bandera de Zero 
+    movwf   contador, 0	;Se mueve el valor de contador display al registro w
+    subwf   PORTC, 0  ;Se resta con el valor del contador tmr0
+    btfsc   STATUS, 2 ;Revisión de la bandera de Zero
+    bsf	    PORTE, 0  ;Si está encendida la bandera, se enciende la alarma
+    btfsc   STATUS, 2 ;Revisión de la bandera de Zero
+    clrf    PORTC  ;Si está encendida la bandera, se reinicia la salida del tmr0
+    btfsc   STATUS, 2  ;Revisión de la bandera de Zero
+    call    reiniciar_tmr0 ;Si está encendida la bandera, se reinicio el tmr0
     return
        
 end
